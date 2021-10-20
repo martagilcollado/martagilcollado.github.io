d8993183558c1632f41059f6625fd1cef6e30366
martagilcollado <92859508+martagilcollado@users.noreply.github.com>
2021-10-20 13:23:03 +0200

    first

---
+++ slider.js
@@ -0,0 +1,15 @@
+let index = 0;
+slider();
+
+function slider(){
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    index++;
+    if (index > slides.length)
+        index = 1;
+    slides[index - 1].style.display = "block";
+    setTimeout(slider, 3000);
+}
+
